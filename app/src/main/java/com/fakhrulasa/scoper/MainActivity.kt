d8c05221292ad9a676ec6cb015a88a2ccd80ccbf
fakhrul <fakhrul.alam@btracsl.com>
2021-10-18 17:50:28 +0600

    -file type added

--- app/src/main/java/com/fakhrulasa/scoper/MainActivity.kt
+++ app/src/main/java/com/fakhrulasa/scoper/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.fakhrulasa.scoper
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.ActivityNotFoundException
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
@@ -13,7 +9,9 @@ import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.fakhrulasa.scoper.Scoper.getPickImageIntent
+import com.fakhrulasa.scoper.lib.FileType
+import com.fakhrulasa.scoper.lib.Scoper.getPickImageIntent
+import com.fakhrulasa.scoper.lib.FileUtil
 import java.io.File
 
 
@@ -43,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun chooseImage() {
-        intentDocument.launch(getPickImageIntent(listOf("image/*", "application/pdf/*")))
+        intentDocument.launch(getPickImageIntent(listOf(FileType.PDF, FileType.IMAGE)))
     }
 
     @SuppressLint("WrongConstant")
@@ -54,27 +52,14 @@ class MainActivity : AppCompatActivity() {
                 var file: File? = FileUtil.from(this, result.data?.data!!)
                 if (file?.extension.equals("pdf", ignoreCase = true)) {
                     if (result.resultCode == Activity.RESULT_OK) {
-                        textName.text = file?.absolutePath
+                        textName.text = file?.name
                         textType.text = file?.extension
                         val file_size = java.lang.String.valueOf(file?.length()!! / 1024).toInt()
                         Toast.makeText(this, file_size.toString(), Toast.LENGTH_SHORT).show()
 
-
-                        if (file.exists()) {
-                            val target = Intent(Intent.ACTION_VIEW)
-                            target.setDataAndType(Uri.fromFile(file), "application/pdf")
-                            target.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
-                            val intent = Intent.createChooser(target, "Open File")
-                            try {
-                                startActivity(intent)
-                            } catch (e: ActivityNotFoundException) {
-                                // Instruct the user to install a PDF reader here, or something
-                            }
-                        }
-
                     }
                 } else {
-                    textName.text = file?.absolutePath
+                    textName.text = file?.name
                     textType.text = file?.extension
                     val file_size = java.lang.String.valueOf(file?.length()!! / 1024).toInt()
                     Toast.makeText(
@@ -82,18 +67,7 @@ class MainActivity : AppCompatActivity() {
                         file_size.toString() + "\n" + file.path,
                         Toast.LENGTH_SHORT
                     ).show()
-                    imageView.visibility = View.VISIBLE
-                    if (file.exists()) {
-                        val target = Intent(Intent.ACTION_VIEW)
-                        target.setDataAndType(Uri.fromFile(file), "image/*")
-                        target.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
-                        val intent = Intent.createChooser(target, "Open File")
-                        try {
-                            startActivity(intent)
-                        } catch (e: ActivityNotFoundException) {
-                            // Instruct the user to install a PDF reader here, or something
-                        }
-                    }
+
 
                 }
             }
---
+++ app/src/main/java/com/fakhrulasa/scoper/lib/FileType.kt
@@ -0,0 +1,12 @@
+package com.fakhrulasa.scoper.lib
+
+
+object FileType {
+    const val IMAGE="image/*"
+    const val PDF="application/pdf/*"
+    const val DOC="application/doc/*"
+    const val DOCX="application/docx/*"
+    const val TEXT="text/plain"
+    const val VIDEO="video/*"
+    const val AUDIO="audio/*"
+}
